e063589cb59634014810a06fb6a245e159515bbb
Martin Færøy <martin.faeroy@gmail.com>
2019-10-01 14:01:38 +0200

    added board

--- classes.py
+++ classes.py
@@ -1,5 +1,16 @@
 class Board():
     def __init__(self, size):
-        self.board = [[0 for i in range(size)] for i in range(size)]     
+        self.board = [[0 for i in range(size)] for i in range(size)]
+    
+    def move(self, direction):
+        pass
+        # TODO
+        # check if move is valid (horizontal, vertical, or diagonal)
+        # check if there is an obstacle in the way.
+        # perform move
 
-current_board = Board(8)
+    def shoot(self, target):
+        pass
+        # TODO
+        # check if target is not already destroyed, or a player is on the target.
+        # Destroy target coordinate.
---
+++ run.py
@@ -0,0 +1,5 @@
+from classes import Board
+
+current_board = Board(8)
+current_board.board[0][0] = 1
+pass
